f02c0572f72f1ccc4d425fc4dd5c04f01f6e8d1b
Dominic Serrano <dmsrojo@gmail.com>
2016-10-05 09:57:14 -0500

    Can now create a board in the database and update and emit the board on each move made

--- client/main.js
+++ client/main.js
@@ -3,18 +3,32 @@
 const socket = io();
 const board = document.querySelector('.board');
 const status = document.querySelector('.status');
+let gameState;
 /////////////////////////////////////////
+
 
 socket.on('connect', () => console.log("User connected"));
 socket.on('disconnect', () => console.log("User disconnected"));
-
-const gameState = [
-  ['','',''],
-  ['','',''],
-  ['','','']
-];
+socket.on('error', console.error);
+socket.on('new game', (game) => {
+  gameState = game.board;
+  //Draw the board at start of game
+  drawBoard(game.board);
+  console.log("Test text", game.board);
+});
+socket.on('move made', (game) => {
+  gameState = game.board;
+  //Draw the board after a move has been made
+  drawBoard(game.board);
+});
 
 let nextPlayer = 'X';
+
+// let boardState = [
+//   ['','',''],
+//   ['','',''],
+//   ['','','']
+// ];
 
 const drawBoard = (boardState) => {
   document.querySelector('.board').innerHTML = `
@@ -41,10 +55,7 @@ const drawBoard = (boardState) => {
 };
 
 
-//Draw the board at start of game
-drawBoard(gameState);
-
-
+//Add event listener to the entire game board
 board.addEventListener('click', evt => {
 
   const col = evt.target.cellIndex;
@@ -53,6 +64,8 @@ board.addEventListener('click', evt => {
   if (gameState[row][col]) {
     return console.log('Cannot move there');
   }
+
+  socket.emit('make move', { row, col })
 
   gameState[row][col] = nextPlayer;
   nextPlayer = nextPlayer === 'X' ? 'O': 'X';
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+mongoose.Promise = Promise;
 const { Server } = require('http');
 const socketio = require('socket.io');
 const app = express();
@@ -29,8 +30,27 @@ mongoose.connect(MONGODB_URL, () => {
   server.listen(PORT, () => console.log("Listening on port number ", PORT));
 
   io.on('connection', (socket) => {
+    console.log("User connected to server", socket.id);
+    //On every connection create a new game board
+    Game.create({
+      board: [ ['','',''], ['','',''], ['','',''] ]
+    })
+    .then((game) => {
+      socket.game = game;
+      socket.emit('new game', game);
+    })
+    .catch((err) => {
+      socket.emit('error', err);
+      console.error(err);
+    });
 
-    console.log("User connected to server", socket.id)
+    socket.on('make move', ({row, col}) => {
+      socket.game.board[row][col] = 'X';
+      socket.game.markModified('board');
+      socket.game.save()
+      .then((game) => socket.emit('move made', game));
+    });
+
 
     socket.on('disconnect', () => {
       console.log("User disconnected from server", socket.id);
